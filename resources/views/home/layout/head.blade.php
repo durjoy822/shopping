8ca6a4161279254d032a462fe3f871a7dfc86a68
Saboj Kumar Shaha <sahaapo@gmail.com>
2023-02-15 10:23:03 +0600

    layout updated

--- resources/views/home/layout/head.blade.php
+++ resources/views/home/layout/head.blade.php
@@ -2,21 +2,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="description" content="oslo">
-    <meta name="keywords" content="oslo">
-    <meta name="author" content="oslo">
-    <link rel="icon" href="{{asset('frontendAsset')}}/assets/images/favicon/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="{{asset('frontendAsset')}}/assets/images/favicon/favicon.png" type="image/x-icon">
-    <link rel="manifest" href="manifest.json">
-    <link rel="icon" href="{{asset('frontendAsset')}}/assets/images/favicon/favicon.png" type="image/x-icon">
-    <link rel="apple-touch-icon" href="{{asset('frontendAsset')}}/assets/images/favicon/favicon.png">
-    <meta name="theme-color" content="#0f8fac">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="apple-mobile-web-app-title" content="Oslo">
-    <meta name="msapplication-TileImage" content="{{asset('frontendAsset')}}/assets/images/favicon/favicon.png">
-    <meta name="msapplication-TileColor" content="#FFFFFF">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <title>Oslo | Home</title>
 
     <!-- Google Jost Font -->
